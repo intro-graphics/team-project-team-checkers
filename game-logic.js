ee15d9fdb27dd82299c597be5f3f5ec1d215b2cf
Tomer Laufer <tomerator@live.com>
2020-05-20 16:48:30 -0700

    created the AI from scratch, which choses a move for black by Minimax

--- game-logic.js
+++ game-logic.js
@@ -1,13 +1,53 @@
+/////////////////////////////////////////////////////////////////////////////////////
+// GLOBALS //
+/////////////////////////////////////////////////////////////////////////////////////
 const BOARD_DIM = 8
+const MAX_HEURISTIC = 100
 
-var test_state = [['b', 0 , 0 , 0 , 0 , 0 , 0 , 0 ],
-                  [ 0 ,'w', 0 , 0 , 0 , 0 , 0 , 0 ],
-                  [ 0 , 0 , 0 , 0 , 0 , 0 , 0,  0 ],
-                  [ 0 , 0 , 0 ,'w', 0 ,'w', 0 , 0 ],
-                  [ 0 , 0 , 0 , 0 , 0 , 0 ,'w', 0 ],
-                  [ 0 ,'w', 0 ,'w', 0 ,'w', 0 , 0 ],
-                  [ 0 , 0 , 0 , 0 ,'w', 0 , 0 , 0 ],
-                  [ 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 ]];
+
+/////////////////////////////////////////////////////////////////////////////////////
+// GAME RULES //
+/////////////////////////////////////////////////////////////////////////////////////
+
+//zig-zag eating
+var test_state1 = [['b', 0 , 0 , 0 , 0 , 0 , 0 , 0 ],
+                   [ 0 ,'w', 0 , 0 , 0 , 0 , 0 , 0 ],
+                   [ 0 , 0 , 0 , 0 , 0 , 0 , 0,  0 ],
+                   [ 0 , 0 , 0 ,'w', 0 ,'w', 0 , 0 ],
+                   [ 0 , 0 , 0 , 0 , 0 , 0 ,'w', 0 ],
+                   [ 0 ,'w', 0 ,'w', 0 ,'w', 0 , 0 ],
+                   [ 0 , 0 , 0 , 0 ,'w', 0 , 0 , 0 ],
+                   [ 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 ]];
+
+//becoming a queen                  
+var test_state2 = [[ 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 ],
+                   [ 0 , 0 ,'b', 0 ,'w', 0 , 0 , 0 ],
+                   [ 0 ,'w', 0 , 0 , 0 , 0 , 0,  0 ],
+                   [ 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 ],
+                   [ 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 ],
+                   [ 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 ],
+                   [ 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 ],
+                   [ 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 ]];
+
+//queen eating backwards
+var test_state3 = [[ 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 ],
+                   [ 0 , 0 , 0 , 0 , 0 , 0 , 0 ,'w'],
+                   [ 0 , 0 ,'w', 0 , 0 , 0 , 0,  0 ],
+                   [ 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 ],
+                   [ 0 , 0 , 0 , 0 ,'w', 0 , 0 , 0 ],
+                   [ 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 ],
+                   [ 0 , 0 , 0 , 0 , 0 , 0 ,'w', 0 ],
+                   [ 0 , 0 , 0 , 0 , 0 , 0 , 0 ,'k']];
+
+//queen chosing to take most peices
+var test_state4 = [[ 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 ],
+                   [ 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 ],
+                   [ 0 , 0 ,'w', 0 , 0 , 0 , 0,  0 ],
+                   [ 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 ],
+                   [ 0 , 0 , 0 , 0 ,'w', 0 ,'w', 0 ],
+                   [ 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 ],
+                   [ 0 , 0 , 0 , 0 , 0 , 0 ,'w', 0 ],
+                   [ 0 , 0 , 0 , 0 , 0 , 0 , 0 ,'k']];
 
 class Game 
 {
@@ -26,7 +66,7 @@ class Game
     this.turn = "white"                                   
   }
 
-  print_state(state=this.gameState)
+  print_state(state)
   {
     var print_str = ""
     for(var r=0; r<BOARD_DIM; r++){
@@ -45,7 +85,7 @@ class Game
      this.print_state(states[i])
   }
 
-  isEnemy(state=this.gameState,turn=this.turn,r,c)
+  isEnemy(state,turn,r,c)
   {
     if(turn=='white')
       return (state[r][c]=='b' || state[r][c]=='k')
@@ -56,7 +96,7 @@ class Game
     return false
   }
 
-  isQueen(state=this.gameState,turn=this.turn,r,c)
+  isQueen(state,turn,r,c)
   {
     if(turn=='white')
       return (state[r][c]=='q')
@@ -67,7 +107,24 @@ class Game
     return false
   }
 
-  copy_state(state=this.gameState)
+  makeQueens(state)
+  {
+    for(var c=0; c<BOARD_DIM; c++)
+    {
+      if (state[0][c] == 'w')
+        state[0][c] = 'q'
+      if (state[BOARD_DIM-1][c] == 'b')
+        state[BOARD_DIM-1][c] = 'k'
+    }
+    return state
+  }
+
+  isEmpty(state,r,c)
+  {
+    return (state[r][c]==0);
+  }
+
+  copy_state(state)
   {
     var copy = [[],[],[],[],[],[],[],[]]
     for(var r=0; r<BOARD_DIM; r++)
@@ -78,30 +135,27 @@ class Game
   }
 
   //swap the peices at these 2 positions (this creates a new state object)
-  swap(state=this.gameState,r1,c1,r2,c2)
+  swap(state,r1,c1,r2,c2)
   {
-   
    var new_state = this.copy_state(state)
    new_state[r1][c1] = state[r2][c2]
    new_state[r2][c2] = state[r1][c1]
-   return new_state;
+   return this.makeQueens(new_state);
   }
 
   //helper for get_eat_states (creates a new state based on an eatin move)
-  eat(state=this.gameState,r1,c1,r2,c2)
+  eat(state,r1,c1,r2,c2)
   {
-   var new_state = this.copy_state(state)
-   new_state[r1][c1] = 0
+   var new_state = this.swap(state,r1,c1,r2,c2)
    new_state[(r1+r2)/2][(c1+c2)/2] = 0
-   new_state[r2][c2] = state[r1][c1]
-   return new_state;
+   return this.makeQueens(new_state);
   }
 
 
-  //traverses (Recursively if needed) from r,c to see all eating moves possible
-  get_eat_states(state=this.gameState, turn=this.turn, r,c, is_queen)
+  //checkes all eating moves (Recursively if needed) from r,c to see all eating moves possible
+  get_eat_states(state, turn, r,c, is_queen)
   {
-      
+    
     //all the eating moves from r,c
     var eat_states = []
 
@@ -112,7 +166,7 @@ class Game
     var bottom_right_allowed = (is_queen||turn=='black')&&(c+2<BOARD_DIM)&&(r+2<BOARD_DIM)
 
     //eat top left
-    if(top_left_allowed && this.isEnemy(state,turn,r-1,c-1) && state[r-2][c-2]==0)
+    if(top_left_allowed && this.isEnemy(state,turn,r-1,c-1) && this.isEmpty(state,r-2,c-2))
     {
       var ate_state = this.eat(state,r,c,r-2,c-2)
       var more_states = this.get_eat_states(ate_state,turn,r-2,c-2,is_queen)
@@ -123,7 +177,7 @@ class Game
     }
 
     //bottom left
-    if(bottom_left_allowed && this.isEnemy(state,turn,r+1,c-1) && state[r+2][c-2]==0)
+    if(bottom_left_allowed && this.isEnemy(state,turn,r+1,c-1) && this.isEmpty(state,r+2,c-2))
     {
       var ate_state = this.eat(state,r,c,r+2,c-2)
       var more_states = this.get_eat_states(ate_state,turn,r+2,c-2,is_queen)
@@ -134,7 +188,7 @@ class Game
     }
 
     //top right
-    if(top_right_allowed && this.isEnemy(state,turn,r-1,c+1) && state[r-2][c+2]==0)
+    if(top_right_allowed && this.isEnemy(state,turn,r-1,c+1) && this.isEmpty(state,r-2,c+2))
     {
       var ate_state = this.eat(state,r,c,r-2,c+2)
       var more_states = this.get_eat_states(ate_state,turn,r-2,c+2,is_queen)
@@ -145,7 +199,7 @@ class Game
     }
 
     //bottom right
-    if(bottom_right_allowed && this.isEnemy(state,turn,r+1,c+1) && state[r+2][c+2]==0)
+    if(bottom_right_allowed && this.isEnemy(state,turn,r+1,c+1) && this.isEmpty(state,r+2,c+2))
     {
       var ate_state = this.eat(state,r,c,r+2,c+2)
       var more_states = this.get_eat_states(ate_state,turn,r+2,c+2,is_queen)
@@ -157,8 +211,60 @@ class Game
     return eat_states
  }
 
-  next_states(state=this.gameState, turn=this.turn)
+   //checkes all 1-peice moves from r,c
+  get_move_states(state, turn, r,c, is_queen)
   {
+      
+    //all the eating moves from r,c
+    var move_states = []
+
+    //bounds check
+    var top_left_allowed = (is_queen||turn=='white')&&(c-1>=0)&&(r-1>=0)
+    var bottom_left_allowed = (is_queen||turn=='black')&&(c-1>=0)&&(r+1<BOARD_DIM)
+    var top_right_allowed = (is_queen||turn=='white')&&(c+1<BOARD_DIM)&&(r-1>=0)
+    var bottom_right_allowed = (is_queen||turn=='black')&&(c+1<BOARD_DIM)&&(r+1<BOARD_DIM)
+
+    //top left
+    if(top_left_allowed && this.isEmpty(state,r-1,c-1))
+      move_states.push(this.swap(state,r,c,r-1,c-1))
+
+    //bottom left
+    if(bottom_left_allowed && this.isEmpty(state,r+1,c-1))
+      move_states.push(this.swap(state,r,c,r+1,c-1))
+
+    //top right
+    if(top_right_allowed && this.isEmpty(state,r-1,c+1))
+      move_states.push(this.swap(state,r,c,r-1,c+1))
+
+    //bottom right
+    if(bottom_right_allowed && this.isEmpty(state,r+1,c+1))
+      move_states.push(this.swap(state,r,c,r+1,c+1))
+    
+    return move_states
+  }
+  
+  //check if game is over
+  is_goal_state(state)
+  {
+    var white_won = true
+    var black_won = true
+    for(var r=0; r<BOARD_DIM; r++){
+      for(var c=0; c<BOARD_DIM; c++){
+        if(this.isEnemy(state,'white',r,c))
+          white_won = false
+        if(this.isEnemy(state,'black',r,c))
+          black_won = false
+        if(!white_won && !black_won)
+          return false
+       }
+    }
+    return true
+  }
+
+
+  next_states(state, turn)
+  {
+         
     //all next_states where we move
     var move_states = []
 
@@ -170,30 +276,22 @@ class Game
       for(var c=0; c<BOARD_DIM; c++){
 
         //item at this coord
-        if( this.isEnemy(state,turn,r,c) )
+        if( this.isEnemy(state,turn,r,c) || this.isEmpty(state,r,c))
           continue;
     
         //eating
-        eat_states.concat(this.get_eat_states(state,turn,r,c,this.isQueen(state,turn,r,c)))
+        var l = this.get_eat_states(state,turn,r,c,this.isQueen(state,turn,r,c))
+        eat_states=eat_states.concat(l)
                 
         //if the option to eat exists, we must eat. We dont need non-eating moves
         if (eat_states.length != 0)
           continue;
 
         //moving
-
-         //left move
-        if ((c>0)&&(r+dir>=0)&&(r+dir<BOARD_DIM)&&(state[r+dir][c-1]==0)){
-          move_states.push(this.swap(state,r,c,r+dir,c-1))
-        }
-      
-        //right move
-        if ((c+1<BOARD_DIM)&&(r+dir>=0)&&(r+dir<BOARD_DIM)&&(state[r+dir][c+1]==0) ){
-          move_states.push(this.swap(state,r,c,r+dir,c+1))
+        var l = this.get_move_states(state,turn,r,c,this.isQueen(state,turn,r,c))
+        move_states = move_states.concat(l)
         }                   
       }
-    }
-
 
     //in checkers we must eat if it is possible:
     if (eat_states.length != 0)
@@ -203,3 +301,104 @@ class Game
   }
 }
 
+/////////////////////////////////////////////////////////////////////////////////////
+// AI //
+/////////////////////////////////////////////////////////////////////////////////////
+
+class Game_AI
+{
+
+  //give the GAME_AI the GAME instance
+  constructor(g)
+  {
+    this.game = g
+    this.gameState = g.gameState
+  }
+
+  //update the gameState
+  update_gameState()
+  {
+    this.gameState = this.g.gameState
+  }  
+
+  //enemy peices left (helper for heuristic)
+  enemies_left(state,turn)
+  {
+    var h = 0
+
+    for(var r=0; r<BOARD_DIM; r++)
+      for(var c=0; c<BOARD_DIM; c++)
+        if(this.game.isEnemy(state,turn,r,c))
+          h += 1
+
+     return h
+  }
+
+  //shortest dist between enemies 
+  enemy_dist(state)
+  {
+    return 0
+  }
+  
+
+  //heuristic: return number of enemy peices left + shortest distance to an enemy
+  heuristic(state,turn)
+  {
+     if(state == null)
+      return MAX_HEURISTIC
+
+     return this.enemies_left(state,turn) + this.enemy_dist(state)
+  }
+
+  //helper for limited_depth_minimax_wrapper
+  limited_depth_minimax(state,turn,l)
+  {
+
+        //depth exceeded or game has ended
+        if(l<=0 || this.game.is_goal_state(state))
+          return state
+
+        //get children_states and check if a goal state was accomplished
+        var children_states = this.game.next_states(state,turn)
+
+        //console.log(l,turn,this.heuristic0(state,turn))
+        //this.game.print_states(children_states)
+
+        //switch turns
+        var next_turn;
+        if (turn == 'white')
+          next_turn = 'black'
+        else
+          next_turn = 'white' 
+
+        //current best
+        var best_state = null  
+        var best_h = MAX_HEURISTIC 
+
+        //for each child recurse deeper
+        for(var i =0; i<children_states.length; i++)
+        {
+          var found_state = this.limited_depth_minimax(children_states[i],next_turn,l-1)
+          var found_h = this.heuristic(found_state,turn)
+          if (found_h < best_h){
+            best_h = found_h
+            best_state = found_state
+          }
+        }
+        return best_state
+  }
+
+  //search
+  limited_depth_minimax_wrapper(state,l)
+  {
+    var best = this.limited_depth_minimax(state,'black',l)
+    //console.log("---")
+    return best
+  }
+
+}
+
+
+var gam = new Game()
+var ai = new Game_AI(gam)
+gam.print_state(ai.limited_depth_minimax_wrapper(test_state4,4))
