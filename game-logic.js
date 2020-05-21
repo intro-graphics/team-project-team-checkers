81f266328313cce30823d5ca3c208fda005487ff
Tomer Laufer <tomerator@live.com>
2020-05-20 18:58:14 -0700

    added heuristics for minimax search

--- game-logic.js
+++ game-logic.js
@@ -3,6 +3,7 @@
 /////////////////////////////////////////////////////////////////////////////////////
 const BOARD_DIM = 8
 const MAX_HEURISTIC = 100
+const MAX_DIST = 10
 
 
 /////////////////////////////////////////////////////////////////////////////////////
@@ -19,7 +20,7 @@ var test_state1 = [['b', 0 , 0 , 0 , 0 , 0 , 0 , 0 ],
                    [ 0 , 0 , 0 , 0 ,'w', 0 , 0 , 0 ],
                    [ 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 ]];
 
-//becoming a queen                  
+//becoming a queen
 var test_state2 = [[ 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 ],
                    [ 0 , 0 ,'b', 0 ,'w', 0 , 0 , 0 ],
                    [ 0 ,'w', 0 , 0 , 0 , 0 , 0,  0 ],
@@ -49,7 +50,7 @@ var test_state4 = [[ 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 ],
                    [ 0 , 0 , 0 , 0 , 0 , 0 ,'w', 0 ],
                    [ 0 , 0 , 0 , 0 , 0 , 0 , 0 ,'k']];
 
-class Game 
+class Game
 {
   //initialize game state
   constructor()
@@ -63,7 +64,7 @@ class Game
                       ['w', 0 ,'w', 0 ,'w', 0, 'w', 0 ],
                       [ 0 ,'w', 0 ,'w', 0 ,'w', 0, 'w'],
                       ['w', 0 ,'w', 0 ,'w', 0, 'w', 0 ]];
-    this.turn = "white"                                   
+    this.turn = "white"
   }
 
   print_state(state)
@@ -83,6 +84,17 @@ class Game
   {
     for (var i=0; i<states.length; i++)
      this.print_state(states[i])
+  }
+
+  isFriendly(state,turn,r,c)
+  {
+    if(turn=='white')
+      return (state[r][c]=='w' || state[r][c]=='q')
+
+    if(turn=='black')
+      return (state[r][c]=='b' || state[r][c]=='k')
+
+    return false
   }
 
   isEnemy(state,turn,r,c)
@@ -130,7 +142,7 @@ class Game
     for(var r=0; r<BOARD_DIM; r++)
       for(var c=0; c<BOARD_DIM; c++)
         copy[r][c] = state[r][c];
-    
+
     return copy;
   }
 
@@ -155,7 +167,7 @@ class Game
   //checkes all eating moves (Recursively if needed) from r,c to see all eating moves possible
   get_eat_states(state, turn, r,c, is_queen)
   {
-    
+
     //all the eating moves from r,c
     var eat_states = []
 
@@ -214,7 +226,7 @@ class Game
    //checkes all 1-peice moves from r,c
   get_move_states(state, turn, r,c, is_queen)
   {
-      
+
     //all the eating moves from r,c
     var move_states = []
 
@@ -239,10 +251,10 @@ class Game
     //bottom right
     if(bottom_right_allowed && this.isEmpty(state,r+1,c+1))
       move_states.push(this.swap(state,r,c,r+1,c+1))
-    
+
     return move_states
   }
-  
+
   //check if game is over
   is_goal_state(state)
   {
@@ -264,25 +276,25 @@ class Game
 
   next_states(state, turn)
   {
-         
+
     //all next_states where we move
     var move_states = []
 
     //all next_states where we eat
-    var eat_states = []    
+    var eat_states = []
 
     //traverse board and look for w or b peices
     for(var r=0; r<BOARD_DIM; r++){
       for(var c=0; c<BOARD_DIM; c++){
 
         //item at this coord
-        if( this.isEnemy(state,turn,r,c) || this.isEmpty(state,r,c))
+        if( !this.isFriendly(state,turn,r,c))
           continue;
-    
+
         //eating
         var l = this.get_eat_states(state,turn,r,c,this.isQueen(state,turn,r,c))
         eat_states=eat_states.concat(l)
-                
+
         //if the option to eat exists, we must eat. We dont need non-eating moves
         if (eat_states.length != 0)
           continue;
@@ -290,7 +302,7 @@ class Game
         //moving
         var l = this.get_move_states(state,turn,r,c,this.isQueen(state,turn,r,c))
         move_states = move_states.concat(l)
-        }                   
+        }
       }
 
     //in checkers we must eat if it is possible:
@@ -319,86 +331,127 @@ class Game_AI
   update_gameState()
   {
     this.gameState = this.g.gameState
-  }  
+  }
 
-  //enemy peices left (helper for heuristic)
-  enemies_left(state,turn)
+
+  //if r2,c2 is an enemy, return zero, if no where to go, return -1
+  find_peices(state,turn)
   {
-    var h = 0
+    var friendlies = []
+    var enemies = []
 
     for(var r=0; r<BOARD_DIM; r++)
       for(var c=0; c<BOARD_DIM; c++)
-        if(this.game.isEnemy(state,turn,r,c))
-          h += 1
+        if(this.game.isFriendly(state,turn,r,c))
+          friendlies.push([r,c])
+        else if(this.game.isEnemy(state,turn,r,c))
+          enemies.push([r,c])
 
-     return h
+    return [friendlies,enemies]
   }
 
-  //shortest dist between enemies 
-  enemy_dist(state)
+  //enemy peices left (helper for heuristic)
+  peices_up(friendlies,enemies)
   {
-    return 0
+    return friendlies.length - enemies.length
   }
-  
+
+  //shortest dist between enemies
+  enemy_dist(friendlies,enemies)
+  {
+
+    //no enemies or no friendlies, the game is over
+    if(friendlies.length==0 || enemies.length==0)
+      return 0
+
+    var shortest_dist = MAX_DIST
+
+    for(var i=0; i<friendlies.length; i++){
+      for(var j=0; j<enemies.length; j++){
+        var delta_r = Math.abs(friendlies[i][0] - enemies[j][0])
+        var delta_c = Math.abs(friendlies[i][1] - enemies[j][1])
+        var diag_dist = Math.max(delta_r,delta_c)-1
+        if (diag_dist < shortest_dist)
+          shortest_dist = diag_dist
+      }
+    }
+    return Math.max(shortest_dist,0)
+
+  }
+
 
   //heuristic: return number of enemy peices left + shortest distance to an enemy
   heuristic(state,turn)
   {
-     if(state == null)
+    if(state == null)
       return MAX_HEURISTIC
 
-     return this.enemies_left(state,turn) + this.enemy_dist(state)
+    var peice_coords = this.find_peices(state,'black')
+    var friendlies = peice_coords[0]
+    var enemies = peice_coords[1]
+
+    var h = this.peices_up(friendlies,enemies) + Math.floor(this.enemy_dist(friendlies,enemies)/2)
+
+    //console.log(turn,h)
+    //this.game.print_state(state)
+
+     return h
   }
 
   //helper for limited_depth_minimax_wrapper
-  limited_depth_minimax(state,turn,l)
+  limited_depth_minimax(state,turn,l,max_l)
   {
 
         //depth exceeded or game has ended
         if(l<=0 || this.game.is_goal_state(state))
-          return state
+          if(l == max_l)
+            return state
+          else
+            return this.heuristic(state,turn)
 
         //get children_states and check if a goal state was accomplished
         var children_states = this.game.next_states(state,turn)
 
-        //console.log(l,turn,this.heuristic0(state,turn))
-        //this.game.print_states(children_states)
 
         //switch turns
         var next_turn;
         if (turn == 'white')
           next_turn = 'black'
         else
-          next_turn = 'white' 
+          next_turn = 'white'
 
         //current best
-        var best_state = null  
-        var best_h = MAX_HEURISTIC 
+        var best_state = null
+        var best_h = MAX_HEURISTIC
 
         //for each child recurse deeper
         for(var i =0; i<children_states.length; i++)
         {
-          var found_state = this.limited_depth_minimax(children_states[i],next_turn,l-1)
-          var found_h = this.heuristic(found_state,turn)
+          var found_h = this.limited_depth_minimax(children_states[i],next_turn,l-1,l)
           if (found_h < best_h){
+            best_state = children_states[i]
             best_h = found_h
-            best_state = found_state
           }
         }
-        return best_state
+        if(l == max_l)
+          return best_state
+        else
+          return best_h
   }
 
   //search
   limited_depth_minimax_wrapper(state,l)
   {
-    var best = this.limited_depth_minimax(state,'black',l)
+    var best = this.limited_depth_minimax(state,'black',l,l)
     //console.log("---")
     return best
   }
 
 }
 
-
+/*
 var gam = new Game()
 var ai = new Game_AI(gam)
-gam.print_state(ai.limited_depth_minimax_wrapper(test_state4,4))
+var move = ai.limited_depth_minimax_wrapper(gam.gameState,5)
+gam.print_state(move)
+*/
